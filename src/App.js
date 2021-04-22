59ceb15e0a044a1559600a072cab23a6a91a61c8
huntu1992 <47385733+huntu1992@users.noreply.github.com>
2021-04-22 17:56:35 +0900

    라우터붙이고 모듈들 다운로드해놨음

--- src/App.js
+++ src/App.js
@@ -1,87 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ml5 from 'ml5';
-import Loader from 'react-loader-spinner';
-import useInterval from '@use-it/interval';
-
-import Princess from './Princess';
-import Chart from './Chart';
-
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import './App.css';
-
-let classifier;
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router';
+import Recognition from './Recognition';
 
 function App() {
-  const videoRef = useRef();
-  const [start, setStart] = useState(false);
-  const [result, setResult] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    classifier = ml5.imageClassifier('./model/model.json', () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-          setLoaded(true);
-        });
-    });
-  }, []);
-
-  useInterval(() => {
-    if (classifier && start) {
-      classifier.classify(videoRef.current, (error, results) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        setResult(results);
-        // console.log(results)
-      });
-    }
-  }, 500);
-
-  const toggle = () => {
-    setStart(!start);
-    setResult([]);
-  };
+  useEffect(() => {}, []);
 
   return (
-    <div className='container'>
-      test
-      <Loader
-        type='Watch'
-        color='#00BFFF'
-        height={200}
-        width={200}
-        visible={!loaded}
-        style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}
-      />
-      <div className='upper'>
-        <div className='capture'>
-          <video
-            ref={videoRef}
-            style={{ transform: 'scale(-1, 1)' }}
-            width='300'
-            height='150'
-          />
-          {loaded && (
-            <button onClick={() => toggle()}>{start ? 'Stop' : 'Start'}</button>
-          )}
-        </div>
-        {result.length > 0 && (
-          <div>
-            <Chart data={result[0]} />
-          </div>
-        )}
-      </div>
-      {result.length > 0 && (
-        <div className='results'>
-          <Princess data={result} />
-        </div>
-      )}
-    </div>
+    <>
+      <div>헤더</div>
+      <Switch>
+        <Route exact path='/' />
+        <Route exact path='/recognition' component={Recognition} />
+      </Switch>
+    </>
   );
 }
 
