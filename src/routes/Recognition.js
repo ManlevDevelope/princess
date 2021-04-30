1a1a0f29f9c06d80826a8be22f1005abf060c4f5
huntu1992 <47385733+huntu1992@users.noreply.github.com>
2021-04-30 14:42:07 +0900

    device number확인

--- src/routes/Recognition.js
+++ src/routes/Recognition.js
@@ -22,10 +22,17 @@ function Recognition() {
   const [start, setStart] = useState(false);
   const [result, setResult] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [devicesContent, setDevicesContent] = useState('');
   const window = useWindowSize();
 
   useEffect(() => {
     classifier = ml5.imageClassifier('./model/model.json', async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const d = [];
+      devices.forEach((device) => {
+        d.push('Found device: ' + JSON.stringify(device) + '\n');
+      });
+      setDevicesContent(d);
       stream = await navigator.mediaDevices
         .getUserMedia({
           video: { facingMode: { exact: 'environment' } },
@@ -79,6 +86,7 @@ function Recognition() {
             <h1>스캔하기</h1>
           </div>
         </div>
+        <div>{devicesContent}</div>
         <div className='webcam'>
           <div className='upper'>
             <div className='capture'>
@@ -98,7 +106,6 @@ function Recognition() {
             </div>
           </div>
         </div>
-
         <div className='camera-bg'>
           {result.length > 0 && <Chart data={result[0]} />}
 
