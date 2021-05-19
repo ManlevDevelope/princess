c87b68bd11ea9d3c5a3e856bf4055c119b45f882
Youngmin Jo <cym19921224@gmail.com>
2021-05-20 00:40:53 +0900

    aws deploy test

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { all, fork, put, takeLatest, delay, call } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 import userSaga from './user';
 
-// axios.defaults.baseURL = 'http://localhost:3080';
-axios.defaults.baseURL = 'https://pockethive-server.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:3080';
+// axios.defaults.baseURL = 'https://pockethive-server.herokuapp.com';
 // axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
